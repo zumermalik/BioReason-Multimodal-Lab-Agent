bae9058875fea4591192cd14fcd2c89b46490bd2
zhq0 <zumermalik48@gmail.com>
2026-03-05 22:15:52 +0500

    Add Navbar, HeroSection, and ChatInterface components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import ChatInterface from './components/ChatInterface'
 
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 flex flex-col font-sans">
+      <Navbar />
+      <main className="flex-grow">
+        <HeroSection />
+        <ChatInterface />
+      </main>
+    </div>
+  )
+}
+
+export default App
