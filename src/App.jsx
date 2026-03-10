36a28cd2692cd5350324a259ccc53fa65fab411e
zhq0 <zumermalik48@gmail.com>
2026-03-10 16:58:35 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from './components/Navbar.jsx'
 import HeroSection from './components/HeroSection.jsx'
 import ChatInterface from './components/ChatInterface.jsx'
-import FinOpsDashboard from './components/FinOpsDashboard.jsx' // <-- We imported it!
-import InfoSections from './components/InfoSections.jsx'
+import FinOpsDashboard from './components/FinOpsDashboard.jsx'
+import AdvancedAnalytics from './components/AdvancedAnalytics.jsx' 
 import Integrations from './components/Integrations.jsx'
+import InfoSections from './components/InfoSections.jsx'
 
 function App() {
   return (
