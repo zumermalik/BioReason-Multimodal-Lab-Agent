bd11b98ff79af2be89d036f479795731d47636b7
zhq0 <zumermalik48@gmail.com>
2026-03-10 12:58:51 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import ChatInterface from './components/ChatInterface'
-import InfoSections from './components/InfoSections'
+import Navbar from './components/Navbar.jsx'
+import HeroSection from './components/HeroSection.jsx'
+import ChatInterface from './components/ChatInterface.jsx'
+import InfoSections from './components/InfoSections.jsx'
 
 function App() {
   return (
-    // REMOVED bg-slate-950 and replaced with our clean off-white
     <div className="min-h-screen bg-[#FAFAFA] flex flex-col font-sans selection:bg-[#D9FA50] selection:text-slate-900">
       <Navbar />
       <main className="flex-grow">
         <HeroSection />
         <ChatInterface />
-        {/* ADDED the new write-up sections below the terminal */}
         <InfoSections />
       </main>
     </div>
